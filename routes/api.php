7aed3c26f59331ced65057d48310a14906bed5f9
pqkhai2401 <pqkhai2401@gmail.com>
2026-06-16 14:59:15 +0700

    clear

--- routes/api.php
+++ routes/api.php
@@ -11,18 +11,11 @@ Route::get('/test', function () {
 
 // Auth
 Route::prefix('auth')->group(function () {
-    Route::post("/login", [AuthController::class, 'login'])->name("api.login");
-    Route::post("/refresh-token", [AuthController::class, 'refreshToken'])->name("api.refresh-token");
-    Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name("api.logout");
-    Route::post("/request-forgot-password", [AuthController::class, 'requestForgotPassword']);
+   
 });
 
 Route::middleware("auth:api")->group(function () {
-    // Image
-    Route::post('/upload-images', [ImageController::class, 'upload']);
-    Route::get('/images', [ImageController::class, 'index']);
-    Route::get('/images/{patientCode}', [ImageController::class, 'show']);
-    Route::delete('/delete-image/{id}', [ImageController::class, 'destroy']);
+    
 });
 
 ?>
