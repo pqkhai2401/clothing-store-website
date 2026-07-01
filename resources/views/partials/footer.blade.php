b36239783cf78d4f56423f52cff2aa42e4eb8bdd
pqkhai2401 <pqkhai2401@gmail.com>
2026-07-01 18:49:58 +0700

    update button trang about

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -33,15 +33,12 @@
                     <li class="text-muted"><i class="bi bi-envelope me-2"></i> #### </li>
                 </ul>
                 <div class="footer-socials mt-4">
-                    <!-- TODO: thay href="#" bằng link trang Facebook của bạn -->
                     <a href="https://www.facebook.com/kha.rea.19" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
                         <img src="{{ asset('images/Facebook.png') }}" alt="Facebook" loading="lazy">
                     </a>
-                    <!-- TODO: thay href="#" bằng link trang Instagram của bạn -->
                     <a href="https://www.instagram.com/kha_rea.19/?hl=en" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
                         <img src="{{ asset('images/Instagram.png') }}" alt="Instagram" loading="lazy">
                     </a>
-                    <!-- TODO: thay href="#" bằng link Zalo (vd: https://zalo.me/xxxxxxxxxx) của bạn -->
                     <a href="https://zalo.me/0357989856" target="_blank" rel="noopener noreferrer" aria-label="Zalo">
                         <img src="{{ asset('images/Zalo.png') }}" alt="Zalo" loading="lazy">
                     </a>
